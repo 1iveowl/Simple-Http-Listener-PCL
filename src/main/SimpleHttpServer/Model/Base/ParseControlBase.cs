771c1240416719be67284ee4ac2826d4f82820ac
1iveowl <liveowl@outlook.com>
2016-11-19 16:34:25 +0100

    Clean-up using

--- src/main/SimpleHttpServer/Model/Base/ParseControlBase.cs
+++ src/main/SimpleHttpServer/Model/Base/ParseControlBase.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ISimpleHttpServer.Model;
+﻿using ISimpleHttpServer.Model;
 using ISocketLite.PCL.Interface;
 
 namespace SimpleHttpServer.Model.Base
