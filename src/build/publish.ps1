42350420b4456f01ec1161ddea6b29c06b03fd4f
1iveowl <liveowl@outlook.com>
2016-11-20 12:28:40 +0100

    Clean-up and added support for local nuget repo

--- src/build/publish.ps1
+++ src/build/publish.ps1
@@ -1,10 +1,10 @@
 $msbuild = join-path -path (Get-ItemProperty "HKLM:\software\Microsoft\MSBuild\ToolsVersions\14.0")."MSBuildToolsPath" -childpath "msbuild.exe"
-&$msbuild ISimpleHttpServer\ISimpleHttpServer.csproj /t:Build /p:Configuration="Release"
-&$msbuild SimpleHttpServer\SimpleHttpServer.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\main\ISimpleHttpServer\ISimpleHttpServer.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\main\SimpleHttpServer\SimpleHttpServer.csproj /t:Build /p:Configuration="Release"
 
-$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path 'SimpleHttpServer\bin\Release\ISimpleHttpServer.dll')).Version.ToString(3)
+$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\SimpleHttpServer\bin\Release\ISimpleHttpServer.dll')).Version.ToString(3)
 
-Remove-Item SimpleHttpServer\NuGet -Force -Recurse
+Remove-Item NuGet -Force -Recurse
 
 New-Item -ItemType Directory -Force -Path .\NuGet
 
---
+++ src/build/publishLocal.ps1
@@ -0,0 +1,13 @@
+$msbuild = join-path -path (Get-ItemProperty "HKLM:\software\Microsoft\MSBuild\ToolsVersions\14.0")."MSBuildToolsPath" -childpath "msbuild.exe"
+&$msbuild ..\main\ISimpleHttpServer\ISimpleHttpServer.csproj /t:Build /p:Configuration="Release"
+&$msbuild ..\main\SimpleHttpServer\SimpleHttpServer.csproj /t:Build /p:Configuration="Release"
+
+$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\SimpleHttpServer\bin\Release\ISimpleHttpServer.dll')).Version.ToString(3)
+
+Remove-Item NuGet -Force -Recurse
+
+New-Item -ItemType Directory -Force -Path .\NuGet
+
+NuGet.exe pack SimpleHttpServer.nuspec -Verbosity detailed -Symbols -OutputDir "NuGet" -Version $version
+
+nuget.exe push -Source "1iveowlNuGetRepo" -ApiKey key .\NuGet\SimpleHttpListener.$version.nupkg
