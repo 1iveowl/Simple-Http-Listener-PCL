fa1b4f056145f59c31965590dcbffb8d6b2714b1
1iveowl <liveowl@outlook.com>
2016-11-23 16:50:11 +0100

    Fixed nuget deployment

--- src/build/publish.ps1
+++ src/build/publish.ps1
@@ -7,4 +7,6 @@ else {
 	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\main\SimpleHttpServer\bin\Release\ISimpleHttpServer.dll')).Version.ToString(3) + "-" + $betaver
 }
 
+.\build.ps1 $version
+
 Nuget.exe push .\Nuget\SimpleHttpListener.$version.nupkg -Source https://www.nuget.org
