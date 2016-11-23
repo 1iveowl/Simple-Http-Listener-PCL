6e80dd7d4f4ee431665000916d2492987fce5d04
1iveowl <liveowl@outlook.com>
2016-11-23 19:20:42 +0100

    Nuget deployment update

--- src/build/build.ps1
+++ src/build/build.ps1
@@ -6,6 +6,7 @@ $msbuild = join-path -path (Get-ItemProperty "HKLM:\software\Microsoft\MSBuild\T
 &$msbuild ..\main\ISimpleHttpServer\ISimpleHttpServer.csproj /t:Build /p:Configuration="Release"
 &$msbuild ..\main\SimpleHttpServer\SimpleHttpServer.csproj /t:Build /p:Configuration="Release"
 
+
 Remove-Item NuGet -Force -Recurse
 
 New-Item -ItemType Directory -Force -Path .\NuGet
