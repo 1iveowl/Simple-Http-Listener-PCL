dc815ca79b0aafa87796182cfeb246d8e756b98f
1iveowl <liveowl@outlook.com>
2017-02-05 13:56:13 +0100

    v.3.6.4-beta1

--- src/build/build.ps1
+++ src/build/build.ps1
@@ -1,3 +1,7 @@
+param([string]$version)
+
+if ([string]::IsNullOrEmpty($version)) {$version = "0.0.1"}
+
 $msbuild = join-path -path (Get-ItemProperty "HKLM:\software\Microsoft\MSBuild\ToolsVersions\14.0")."MSBuildToolsPath" -childpath "msbuild.exe"
 &$msbuild ..\main\ISimpleHttpServer\ISimpleHttpServer.csproj /t:Build /p:Configuration="Release"
 &$msbuild ..\main\SimpleHttpServer\SimpleHttpServer.csproj /t:Build /p:Configuration="Release"
@@ -6,3 +10,4 @@ Remove-Item NuGet -Force -Recurse
 
 New-Item -ItemType Directory -Force -Path .\NuGet
 
+NuGet.exe pack SimpleHttpServer.nuspec -Verbosity detailed -Symbols -OutputDir "NuGet" -Version $version
